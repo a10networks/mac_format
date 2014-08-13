db72ddcb8b2f37aa600abdece9a3fc966392fc00
John Otander <johnotander@gmail.com>
2014-08-13 12:15:38 -0600

    Add dependencies.

--- lib/mac_format.rb
+++ lib/mac_format.rb
@@ -1,5 +1,10 @@
+require "mac_address"
 require "mac_format/version"
+require "mac_format/mac_format_validator"
 
 module MacFormat
-  # Your code goes here...
+
+  def self.valid?(mac)
+    mac.to_mac.valid_mac?(strict: true)
+  end
 end
